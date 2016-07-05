f9d6ab7dda86fbd69f93131ae18b974734abc1a6
Jimmy Chen <chen.s.jimmy@gmail.com>
2016-07-04 21:57:42 -0600

    fixed routing again

--- js/controllers/App.js
+++ js/controllers/App.js
@@ -1,9 +1,13 @@
-var app=angular.module('App',['ngRoute','CarouselCtrl', 'NavBarCtrl','ArticleListCtrl', 'ArticleCtrl']);
+var app=angular.module('App',['ngRoute', 'NavBarCtrl', 'CarouselCtrl','ArticleListCtrl', 'ArticleCtrl']);
 app.config(['$routeProvider', function($routeProvider, $http){
 	$routeProvider.when('/', {
 		templateUrl : 'main.html'
 	}).
 	when('/articles/:param', {
-		templateUrl : 'article.html'
-	});
+		templateUrl : 'article.html',
+		controller:'ArticleCtrl'
+	}).
+	otherwise({
+    	redirectTo: '/'
+  	});
 }]);
